33f34546b13a378c314b474b645d3e1ef0369b3f
Gabriel Julian <gluisjulian@gmail.com>
2023-11-07 21:06:21 -0300

    Adicionado os Pacotes do Nuget, criação do contexto e migrations do identity.

--- IOStore/IOStore.Identidade.API/Program.cs
+++ IOStore/IOStore.Identidade.API/Program.cs
@@ -1,6 +1,23 @@
+using IOStore.Identidade.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+//Context
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Context do Identity
+builder.Services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
